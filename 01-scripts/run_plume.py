40a34b8cb9c6c885b6046a95285f8577f07234e0
amel <amel.derras@gmail.com>
2022-11-04 17:23:46 -0400

    corrected names in save function

--- 01-scripts/run_plume.py
+++ 01-scripts/run_plume.py
@@ -14,10 +14,11 @@ if __name__ == "__main__":
     storage = spm.initialize_storage(len(sounding[0]),len(constants.entrT_list))
 
     sol = spm.run_single_plume(storage, sounding,  assume_entr=True)
-    helpers.save_vec_elems_as_csv(sounding[:7], ["z","p","t","sh","rho"])
+
+    helpers.save_vec_elems_as_csv(sounding[:7], ["z","p","t","sh","mse_a", "mse_as","rho"])
     helpers.save_vec_elems_as_csv(sol,
             ["w_c", "mse_c", "q_w", "t_c", "B", "mflux", "entr", "detr", "t_va",\
-            "t_vc", "q_i", "q_va", "q_vc", "entrT_list"])
+            "t_vc", "q_i", "q_va", "q_vc", "entrT"])
 
     sol_weighted = wf.get_weighted_profile(sol, sounding, cth=10.0)
     helpers.save_dict_elems_as_csv(sol_weighted, suffix="_weighted")
