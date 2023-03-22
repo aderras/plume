71db375912d9dae8135d8efde1953987c60dbedd
amel <amel.derras@gmail.com>
2023-03-22 16:30:12 -0400

    reorganized the file and changed to a trapezoidal integration scheme

--- 01-scripts/weighting_functions.py
+++ 01-scripts/weighting_functions.py
@@ -2,160 +2,165 @@ import numpy as np
 import constants
 import pickle
 from scipy.interpolate import interp1d
+from numba import njit
 
-
-def import_climatology_ctb(in_cth=10.0):
-    """Getting a climatological CTB given a CTH"""
-    CLIMATOLOGY_CTB = pickle.load(open('/home/amel/Documents/plume-model/02-data/in/climatology_all.pkl','rb'))
-
-    cth = CLIMATOLOGY_CTB['cth']
-    ctb = CLIMATOLOGY_CTB['ctb']
-
-    f = interp1d(cth, ctb)
-
-    try:
-        out_ctb = f(in_cth)
-    except ValueError:
-        return np.nan
-
-    return out_ctb
-
+@njit()
 def compute_ta(height, temperature, cth):
+    """
+    Compute the ambient temperature at the cloud top given the cloud top
+    height and the sounding height and temperature.
+
+    in: height is a 1D array of floats, temperature is a 1D array of floats,
+    cth is a float.
+
+    out: a float representing the ambient temperature at height cth
+    """
     z = np.abs(height - cth)
-    ind = np.nanargmin(z)
+    minval = min(z)
+    ind = np.where(z==minval)[0][0]
     ta = temperature[ind]
     return ta
 
-def get_weighted_profile(results, sounding, cth=10.0):
-    ''' Wrapper code to run the weighting of SPM_outputs given the spm_output_dictionary
-    input:
-        single plume model output from cm.run_single_plume(), Should be a dictionary
-        cth -- height in km (cth)
-        tc -- cloud top temperature in K
-        ta -- ambient temperature in K
-    '''
+@njit()
+def integrate_trapezoidal(f, h, n):
+    """
+    Compute the discrete integral of a vector f using the trapezoidal rule
 
-    z_sounding = sounding[0]
-    t_sounding = sounding[2]
+    in: f is a 1D array, h is a float representing the horizontal spacing, n is
+    an integer representing the length of f.
 
-    ctb = import_climatology_ctb(cth)
-    ta = compute_ta(z_sounding, t_sounding, cth)
-    tc = ta * ctb + ta
+    out: a float representing the integral of the vector.
+    """
+    s = 0.0
+    s += f[0]/2.0
+    for i in range(1, n-1):
+        s += f[i]
+    s += f[-1]/2.0
+    return s * h
 
-    zt = cth
-    dtt = (tc-ta)/ta
+@njit()
+def compute_weighted_prob(m_ΔT, m_wc, m_z, cth, tc, ta):
+    """
+    Compute Eq. (21) of ML16.
 
-    w_c, mse_c, mr_w, t_c, B, mflux, entr, detr, t_va, t_vc, mr_i, \
-            mr_va, mr_vc, entrT_list = results
 
-    m_w = w_c
-    m_tvc = t_vc
-    m_tva = t_va
-    m_entrM = entr
-    m_detrM = detr
-    m_entr = entrT_list
+    """
 
-    m_z = z_sounding/1e3 # Have to convert to km
+    Δz = m_z[1]-m_z[0] # Get the horizontal spacing, Δz
+    num_z = m_ΔT.shape[0] # Get the number of levels that we consider
+    num_entr = m_ΔT.shape[1] # Get the number of levels that we consider
 
-    # makding sure the division is only done for values that are valid
-    valid_ind = (~np.isnan(m_tvc)) & (~np.isnan(m_tva)) & ~(m_tva == 0.)
-    m_dt = np.zeros(m_tvc.shape)
+    ############################################################################
+    # Begin probability computation (calculating Eq. (21))
 
-    for i in range(m_dt.shape[0]):
-        for j in range(m_dt.shape[1]):
-            if (valid_ind[i, j]):
-                m_dt[i, j] = (m_tvc[i, j] - m_tva[i, j])/m_tva[i, j]
-            if (np.isnan(m_entrM[i, j])):
-                m_entrM[i, j] = 0.
-            if (np.isnan(m_detrM[i, j])):
-                m_detrM[i, j] = 0.
+    ## Arguments of the exponential function :
+    σz = 1.0 # Assume error in z is 1 km.
+    σΔT = (1.0/ta) # Assume error in ΔT is 1 Kelvin/temperature at the top
+    zt = cth # Cloud top height
+    ΔT = (tc-ta)/ta # Normalized pressure difference of at the top of the cloud
 
-    # computing sigma T -- measurement errors
-    sigma_dt = 2. * (1./ta)**2
-    sigma_z = 2. * (1.)**2
 
-    # setting zeros for final weighted profile
-    mean_w = np.zeros(m_w.shape[0])
-    mean_dt = np.zeros(m_w.shape[0])
-    mean_entr = np.zeros(m_w.shape[0])
-    mean_detr = np.zeros(m_w.shape[0])
-
-    # probabilty initialization to zeros
-    p_norm = 0
-    p_arr = np.zeros(m_entr.shape[0])
-
-    h_cond_grid = np.zeros((m_z.shape[0], m_entr.shape[0]))
+    integrand = np.zeros(num_z)
+    p_norm = 0.0
+    p_arr = np.zeros(num_z)
 
     # loop through the entrainment rate
-    for entr_ind in np.arange(0, m_entr.shape[0]):
+    for entr_ind in np.arange(num_entr):
 
-        # getting the model profiles for a given entr rate
-        # entr_val = m_entr[entr_ind]
-        m_wi = m_w[:, entr_ind]
-        m_dti = m_dt[:, entr_ind]
+        # Get the vertical velocity and the ΔT for this entrainment rate
+        m_wi = m_wc[:, entr_ind]
+        m_ΔTi = m_ΔT[:, entr_ind]
 
-        # finding out where w_c goes to zero or vanishes
-        ind = np.argwhere(~np.isnan(m_wi) & (m_wi != 0.0))
+        # Determine valid heights as the index where w_c is finite and nonzero
+        idx_valid_wc = np.argwhere(~np.isnan(m_wi) & (m_wi != 0.0))
+        cb_ind = np.nanmin(idx_valid_wc) # Index of the cloud bottom
+        ct_ind = np.nanmax(idx_valid_wc)+1 # Index of the cloud top
 
-        # finding index of cloud bottom and cloud top, aka have w_c values > 0
-        cb_ind = np.nanmin(ind)
-        ct_ind = np.nanmax(ind)+1
-        # print("cb_ind = ",cb_ind, ", ct_ind = ", ct_ind)
-
-        # looping through the cloud top and bottom
-        temp_sum = 0
-        h_cond_grid[cb_ind:ct_ind, entr_ind] = 1
+        # Compute the integral in Eq. (21) using the trapezoidal rule. First
+        # fill the array `integrand` with values of the integrand for all z
+        for k in range(num_z): integrand[k]=0.0
         for k in np.arange(cb_ind, ct_ind):
+            integrand[k] = np.exp((-(zt - m_z[k])**2)/(2.0*σz**2) - \
+                                    (ΔT - m_ΔTi[k])**2/(2.0*σΔT**2))
+        intgrl = integrate_trapezoidal(integrand,Δz,num_z)
 
-            # computing the integration from cloud bottom to cloud top, eq 21
-            e_z = np.exp((-1.*(zt - m_z[k])**2)/(sigma_z))
-            e_dt = np.exp((-1.*(dtt - m_dti[k])**2)/(sigma_dt))
-            temp_sum += e_z * e_dt
+        # The probability for this entrainment rate is the integral times
+        # 1/(length of integration). Store the result in p_arr
+        p_arr[entr_ind] = (1.0/(m_z[ct_ind] - m_z[cb_ind]))*intgrl
 
-        # final prob of obs for a given entr rate
-        p_obs_entr = (1./(ct_ind - cb_ind)) * (temp_sum)
+        # Sum all of the probabilities in order to compute the normalization
+        p_norm += p_arr[entr_ind]
 
-        p_arr[entr_ind] = p_obs_entr
+    # Normalize all of the probabilites
+    return p_arr/p_norm
 
-        # sum up the prob of all obs given entr
-        p_norm += p_obs_entr
 
-    cnt = 0
-    pr = np.ones(m_entr.shape[0])/m_entr.shape[0]
-    p_arr = p_arr/p_norm
+@njit()
+def get_weighted_profile(results, sounding, cth=10.0, ctb=0.019224):
 
-    while (cnt < 100):
-        pr = pr * p_arr
-        pr = pr / np.nansum(pr)
-        cnt = cnt + 1
+    # Unpack the results
+    m_wc, mse_c, mr_w, t_c, B, mflux, m_entrM, m_detrM, m_tva, m_tvc, mr_i, \
+            mr_va, mr_vc, m_entr = results
 
-    mean_w = np.zeros((m_z.shape[0], ))
-    mean_entr = np.zeros((m_z.shape[0], ))
-    mean_detr = np.zeros((m_z.shape[0], ))
-    mean_dt = np.zeros((m_z.shape[0], ))
+    num_z = m_wc.shape[0] # Get the number of levels that we consider
+    num_entr = m_wc.shape[1] # Get the number of levels that we consider
 
-    for i in range(m_w.shape[1]):
+    ############################################################################
+    ## Compute the temperature at the top of the cloud
 
-        # finding out where w_c goes to zero or vanishes
-        ind = np.argwhere(~np.isnan(m_w[:, i]) & (m_w[:, i] != 0.0))
+    m_z = sounding[0]/1e3 # Convert sounding height from m to km
+    t_sounding = sounding[2]
+
+    # Compute the ambient temperature at the top of the cloud.
+    ta = compute_ta(m_z, t_sounding, cth)
+
+    # The cloud top temperature is the ambient temperature plus a funcion of
+    # the cloud top buoyancy.
+    tc = ta * ctb + ta
+
+    ############################################################################
+    ## Compute ΔT = (T_c - T_a)/T_a
+
+    m_ΔT = np.zeros(m_tvc.shape)
+    for i in range(num_z):
+        for j in range(num_entr):
+            if (~np.isnan(m_tvc[i,j])) and (~np.isnan(m_tva[i,j])) and m_tva[i,j]!=0:
+                m_ΔT[i, j] = (m_tvc[i, j] - m_tva[i, j])/m_tva[i, j]
+
+    ############################################################################
+    ## Compute the weighted probability function for all entrainment rates
+
+    pr = compute_weighted_prob(m_ΔT,m_wc, m_z, cth, tc, ta)
+
+    ############################################################################
+    # Compute expected values for the observables of interest
+    # to compute another variable:
+    #    - create a new array of zeros of length num_z
+    #    - add the array to all_obs
+    #    - add the observable's corresponding plume model output to all_raw
+    mean_w = np.zeros((num_z, ))
+    mean_entr = np.zeros((num_z, ))
+    mean_detr = np.zeros((num_z, ))
+    mean_dt = np.zeros((num_z, ))
+
+    all_obs = [mean_w, mean_entr, mean_detr, mean_dt]
+    all_raw = [m_wc, m_entrM, m_detrM, m_ΔT]
+
+    # Sum the probability of
+    for i in range(num_entr):
 
         # finding index of cloud bottom and cloud top, aka have w_c values > 0
-        cb_ind = np.nanmin(ind)
-        ct_ind = np.nanmax(ind)+1
+        idx_valid_wc = np.argwhere(~np.isnan(m_wc[:, i]) & (m_wc[:, i] != 0.0))
+        cb_ind = np.nanmin(idx_valid_wc)
+        ct_ind = np.nanmax(idx_valid_wc)+1
 
-        for n in range(cb_ind, ct_ind):
-            mean_w[n] += m_w[n, i] * pr[i] * h_cond_grid[n, i]
-            mean_entr[n] += m_entrM[n, i] * pr[i] * h_cond_grid[n, i]
-            mean_detr[n] += m_detrM[n, i] * pr[i] * h_cond_grid[n, i]
-            mean_dt[n] += m_dt[n, i] * pr[i] * h_cond_grid[n, i]
+        for idobs, obs in enumerate(all_obs):
+            obs_raw = all_raw[idobs]
+            for idz in range(cb_ind,ct_ind):
+                obs[idz] += obs_raw[idz, i]*pr[i]
 
-    mean_entr[mean_w <= 0.0] = np.nan
-    mean_entr[mean_w == np.nan] = np.nan
-    mean_detr[mean_w <= 0.0] = np.nan
-    mean_detr[mean_w == np.nan] = np.nan
-    mean_dt[mean_w <= 0.0] = np.nan
-    mean_dt[mean_w == np.nan] = np.nan
-    mean_w[mean_w <= 0.0] = np.nan
+    # When all of the observables are computed, replace the areas where mean_w
+    # is zero or nan with nan.
+    for obs in all_obs: obs[(mean_w <=0) ^ (np.isnan(mean_w))] = np.nan
 
-    return {'w_c': mean_w, 'dt': mean_dt, 'entr': mean_entr, 'detr': mean_detr,
-            'pr': pr, 'pr_orig': p_arr, 'h_cond': h_cond_grid}
+    return (mean_w, mean_dt, mean_entr, mean_detr, pr)
